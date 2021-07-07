760d1cca4fcf8e7ce843ef3590a5e9e6c4a0a825
IvetteAb <85627950+IvetteAb@users.noreply.github.com>
2021-07-07 19:11:09 +0100

    Update Random password generator.py

--- Random password generator.py
+++ Random password generator.py
@@ -4,12 +4,12 @@
 from random import randint
 
 # create an all uppercase password,
-# start w/ an empty string --> L11
-# to figure out how long you want the password to be, for loop --> L12
-# random integer between 65 & 90, which corresponds to a letter in the alphabet --> L13
-# take the original password, make it equal to a string then add i (random letter) --> 14
+# start with an empty string --> L14
+# to figure out how long you want the password to be, for loop --> L15
+# random integer between 65 & 90, which corresponds to a letter in the alphabet --> L16
+# take the original password, make it equal to a string then add i (random letter) --> 17
 # it'll repeat 12 times & add a random letter each time
-# password = the string of the pw (which began as an empty string, L14) + uppercase (i) on loop * 10
+# password = the string of the password (which began as an empty string, L17) + uppercase (i) on loop * 10
 # to make it lowercase --> i = chr(randint(65, 90)).lower()
 password = ""
 for i in range(12):
@@ -17,10 +17,10 @@ for i in range(12):
     password = str(password) + i
 print(password)
 
-# upper & lower case alternating password --> 10 letters long, random letters
+# upper case & lower case alternating password --> 10 letters long, random letters
 # uppercase characters --> L25/26 & lowercase characters --> L27/28
 # ij loop * 5
-# pw = string of the pw (which began as an empty string, L23) + uppercase (i) & then + lowercase (j) loop, L29
+# password = string of the password (which began as an empty string, L24) + uppercase (i) & then + lowercase (j) loop, L29
 password2 = ""
 for i in range(5):
     i = chr(randint(65, 90))
@@ -30,9 +30,9 @@ for i in range(5):
 print(password2)
 
 # all lowercase password
-# password = the string of the pw (which began as an empty string, L34) + uppercase (i) on loop * 16
-password = ""
+# password = the string of the password (which began as an empty string, L34) + uppercase (i) on loop * 16
+password3 = ""
 for i in range(16):
     i = chr(randint(65, 90)).lower()
-    password = str(password) + i
-print(password)
+    password3 = str(password3) + i
+print(password3)
